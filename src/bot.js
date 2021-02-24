75631bcad2dc45bbb7c15ecb9a75930e8b293fa9
Patrick Starliper <patrickstarliper@gmail.com>
2021-02-23 18:05:56 -0600

    Add clear command to delete all messages in a channel

--- src/bot.js
+++ src/bot.js
@@ -4,11 +4,34 @@ dotenv.config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-
+const PREFIX = '!';
 
 client.once('ready', () => {
-    console.log('Ready.');
+    //console.log('Successfully logged into client.');
+    console.log(`${client.user.username} logged into client.`);
 });
+
+client.on('message', async (message) => {
+    if (message.author.bot) { return; }
+    if (message.content.toLowerCase().startsWith(PREFIX)) {
+        const [command, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);
+
+        if (message.member.roles.cache.find(r => r.name === 'Hutt')) {
+            if (command === 'clear') {
+                let deleted;
+                do {
+                    deleted = await message.channel.bulkDelete(99);
+                } while (deleted.size != 0);
+            }
+        }
+    }
+});
+
+
+
+
 
 client.login(process.env.TOKEN);
